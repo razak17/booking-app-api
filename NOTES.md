5eefd54d0e63cd4e7da3e1a1e37e90ef4711b26a
Razak <moabdulrazak17@gmail.com>
2022-09-19 03:12:09 +0000

    chore: connect db

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,3 @@
-yarn add argon2 cookie-parser cors dotenv express jsonwebtoken zod zod-express-middleware http-status-codes
+yarn add mongoose argon2 cookie-parser cors dotenv express jsonwebtoken zod zod-express-middleware http-status-codes pino pino-pretty dayjs
 
 yarn add -D @typegoose/typegoose @types/cookie-parser @types/cors @types/express @types/jsonwebtoken @types/node ts-node-dev
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log("Hello Mom!")
+import express from "express";
+import cors from "cors";
+import { connect } from "./utils/database";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const main = async () => {
+  const port = process.env.PORT;
+  const app = express();
+
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
+
+  app.listen(port, async () => {
+    console.log(`server started on http://localhost:${port}`);
+    await connect();
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
+
